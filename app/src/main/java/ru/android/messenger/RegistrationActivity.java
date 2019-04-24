ee29eb93f7a766c32c7aef287591005dd802592c
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-04-24 18:00:10 +0700

    Наработки

---
+++ app/src/main/java/ru/android/messenger/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package ru.android.messenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
