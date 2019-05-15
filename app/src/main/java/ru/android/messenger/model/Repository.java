fb12064a99c768f3889fd379a7329b816e34b832
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-05-16 04:15:02 +0700

    Запрос на получние фото пользователя

--- app/src/main/java/ru/android/messenger/model/Repository.java
+++ app/src/main/java/ru/android/messenger/model/Repository.java
@@ -1,6 +1,7 @@
 package ru.android.messenger.model;
 
 import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -8,6 +9,7 @@ import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 import retrofit2.http.Path;
+import retrofit2.http.Streaming;
 import ru.android.messenger.model.api.LoginResponse;
 import ru.android.messenger.model.api.RegistrationResponse;
 import ru.android.messenger.model.dto.User;
@@ -28,4 +30,8 @@ public interface Repository {
 
     @GET("/api/checkAuthenticationToken/{authenticationToken}")
     Call<Boolean> checkAuthenticationToken(@Path("authenticationToken") String authenticationToken);
+
+    @Streaming
+    @GET("/api/getUserPhoto/{login}")
+    Call<ResponseBody> getUserPhoto(@Path("login") String login);
 }
