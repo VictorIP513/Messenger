42798e86d1eff4cb2506d61dc1ba7c71126db698
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-06-11 01:12:24 +0700

    Обновление списка друзей

--- app/src/main/java/ru/android/messenger/view/activity/FriendListActivity.java
+++ app/src/main/java/ru/android/messenger/view/activity/FriendListActivity.java
@@ -21,11 +21,13 @@ public class FriendListActivity extends ActivityWithNavigationDrawer {
         super.init(R.layout.activity_friend_list);
 
         findViews();
+        createFragmentPagerAdapter();
+    }
 
-        FriendsFragmentPagerAdapter adapter =
-                new FriendsFragmentPagerAdapter(getSupportFragmentManager(), this);
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        createFragmentPagerAdapter();
     }
 
     public void actionButtonAddFriendClick(View view) {
@@ -36,5 +38,19 @@ public class FriendListActivity extends ActivityWithNavigationDrawer {
     private void findViews() {
         viewPager = findViewById(R.id.view_pager);
         tabLayout = findViewById(R.id.tabs);
+    }
+
+    private void createFragmentPagerAdapter() {
+        FriendsFragmentPagerAdapter adapter =
+                new FriendsFragmentPagerAdapter(getSupportFragmentManager(), this);
+        if (viewPager.getAdapter() != null) {
+            int currentItem = viewPager.getCurrentItem();
+            viewPager.setAdapter(adapter);
+            tabLayout.setupWithViewPager(viewPager);
+            viewPager.setCurrentItem(currentItem);
+        } else {
+            viewPager.setAdapter(adapter);
+            tabLayout.setupWithViewPager(viewPager);
+        }
     }
 }
